7d35e2d4366e31a8387ea68b222a6cccff8b72f1
Arjun Arundiyar <ramesharjun287@gmail.com>
2025-03-01 02:34:07 +0530

    fully functing code

--- ott_show_application.py
+++ ott_show_application.py
@@ -1,6 +1,8 @@
 import requests  # Import the requests library to make HTTP requests
 
 
+
+
 def show_query():
     title = input(str("Enter the title of the movie: "))  # Prompt the user to enter the title of the movie
     try:
@@ -22,14 +24,26 @@ def show_query():
         # Parse the response JSON data
         data = response.json()
 
-        return data    
+        title = data[0].get("title")
+        overView = data[0].get("overview")
+        relesed_year = data[0].get("releaseYear")
+        rating = data[0].get("rating")
+        images = data[0]["imageSet"]["horizontalPoster"].get("w1440")
+
+        for i in range(len(data[0]["streamingOptions"]["in"])):
+            streaming_type = data[0]["streamingOptions"]["in"][i].get('type')
+            if streaming_type == "subscription":
+                streaming_options = data[0]["streamingOptions"]["in"][i].get("link")
+            else:
+                streaming_options = "Not Available"
+        return title,overView,relesed_year,rating,images,streaming_options
 
     except Exception as e:
         print (e)
 
 # Call the map_query function and store the result in the map variable
-map = show_query()
+movie = show_query()
 
 # Print the result of the API request
-print(map)
+print(movie)
 
